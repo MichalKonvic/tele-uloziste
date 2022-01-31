3cc370c1ce423c0142bd6195b26bff8fae35986e
MichalKonvic <51423281+MichalKonvic@users.noreply.github.com>
2022-01-31 13:27:01 +0100

    codeGrabber back button

--- components/LoginPage/CodeGrabber.tsx
+++ components/LoginPage/CodeGrabber.tsx
@@ -8,7 +8,8 @@ interface ComponentProps{
     ) => void,
     handleAccountChange: (
         e:React.MouseEvent<HTMLButtonElement,MouseEvent>
-    ) => void
+    ) => void,
+    remainingTime: number
 }
 
 
@@ -104,13 +105,24 @@ const CodeGrabber = (
                 <input id="4" onKeyPress={(e) => valueCheck(e)} type="tel" autoCorrect='off' className='duration-300 w-12 h-12 border-2 text-center text-2xl font-bold rounded-xl outline-none appearance-none' />
                 <input id="5" onKeyPress={(e) => valueCheck(e)} type="tel" autoCorrect='off' className='duration-300 w-12 h-12 border-2 text-center text-2xl font-bold rounded-xl outline-none appearance-none' />
             </div>
-            <input type="submit" onClick={(e) => {
-                handleSubmit(e,collectCodes(), codesDivRef);
-            }} value="Pokračovat" className='mt-2 text-2xl py-2 cursor-pointer px-5 bg-violet-500 text-white rounded-lg duration-300 hover:bg-violet-600' />
+            <p className='w-full text-right mr-3 mt-1 text-lg text-gray-800'>4:48</p>
+            <div className='flex items-center w-full justify-between px-5'>
+                <button onClick={(e) => handleAccountChange(e)} className='border-4 border-violet-500 text-white rounded-full w-12 h-12 flex box-border pl-3 items-center hover:border-violet-600 duration-200'><BackArrow/></button>
+                <input type="submit" onClick={(e) => {
+                    handleSubmit(e,collectCodes(), codesDivRef);
+                }} value="Pokračovat" className='mt-2 text-2xl py-2 cursor-pointer px-5 h-12 bg-violet-500 text-white rounded-lg duration-300 hover:bg-violet-600' />
+            </div>
         </div>
     );
 
 };
 
+const BackArrow = () => {
+    return(
+        <svg className='fill-violet-500 hover:fill-violet-600 duration-200' width="14" height="24" viewBox="0 0 14 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0.93934 10.9393C0.353554 11.5251 0.353553 12.4749 0.939339 13.0607L10.4853 22.6066C11.0711 23.1924 12.0208 23.1924 12.6066 22.6066C13.1924 22.0208 13.1924 21.0711 12.6066 20.4853L4.12132 12L12.6066 3.51472C13.1924 2.92894 13.1924 1.97919 12.6066 1.3934C12.0208 0.807615 11.0711 0.807615 10.4853 1.3934L0.93934 10.9393ZM3 10.5L2 10.5L2 13.5L3 13.5L3 10.5Z"/>
+        </svg>
+    )
+}
 
 export default CodeGrabber;
