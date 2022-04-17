7f2968c8e80866a11b5b7efc58d24be30eea2010
MichalKonvic <51423281+MichalKonvic@users.noreply.github.com>
2022-04-17 15:00:27 +0200

    create api route user validation

--- pages/api/data/create.ts
+++ pages/api/data/create.ts
@@ -1,9 +1,14 @@
 import { NextApiResponse, NextApiRequest } from "next";
-import isTokenValid from "../../../lib/tokenValidate";
+import { serverURL } from "../../../config";
+import Data from "../../../models/data";
+import { serverError } from "../../../lib/apiResponseTemplates";
+import dbConnect from "../../../lib/dbConnect";
+import getTokenPayload from "../../../lib/tokenValidate";
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    await dbConnect();
     const { token, data } = req.body;
     if (!token) {
         res.status(400).json({
@@ -19,14 +24,44 @@ export default async function handler(
         });
         return;
     }
-    const isValid = isTokenValid(token);
-    if (!isValid) {
+    const tokenEmail = getTokenPayload(token);
+    if (!tokenEmail) {
         res.status(401).json({
             statusCode: 401,
             message: "Unauthorized"
         });
         return;
     }
+    // Obtain user data from db request
+    const getUserResponse = await (await fetch(`${serverURL}/api/db/getUser`, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${process.env.API_KEY}`,
+            'Content-type':"application/json"
+        },
+        body: JSON.stringify({
+            email: tokenEmail
+        })
+    })).json();
+    if (getUserResponse?.statusCode !== 200) {
+        if (getUserResponse?.statusCode === 404) {
+            res.status(404).json({
+                statusCode: 404,
+                message: "Uživatel nebyl nalezen v databázi"
+            });
+            return;
+        }
+        if (getUserResponse?.statusCode === 401) {
+            console.error("Špatný API klíč (Zkontrolujte .env soubor)");
+        }
+        serverError(res);
+        return;
+    }
+    if (!getUserResponse?.data) {
+        serverError(res);
+        return;
+    }
+    const { id, email } = getUserResponse?.data;
     // TODO create database data
     
 }
